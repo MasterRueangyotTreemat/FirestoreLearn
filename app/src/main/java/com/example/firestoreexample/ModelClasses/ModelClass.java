38bbcb7103cbd0354ff9963f5e2e3cdd31860806
Rueangyot Treemat <ruengyod1@gmail.com>
2019-12-21 14:49:03 +0700

    Model Class for RecyclerView Status Updating App

---
+++ app/src/main/java/com/example/firestoreexample/ModelClasses/ModelClass.java
@@ -0,0 +1,21 @@
+package com.example.firestoreexample.ModelClasses;
+
+public class ModelClass {
+
+    private String status;
+
+    public ModelClass() {
+    }
+
+    public ModelClass(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
